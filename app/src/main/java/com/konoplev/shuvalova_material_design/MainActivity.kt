399ec12fecfd62c35a597daf406d4de53f6e2d6d
dazzes4545 <dazzes7619@gmail.com>
2026-03-04 08:46:40 +0300

    Initial Commit

---
+++ app/src/main/java/com/konoplev/shuvalova_material_design/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.konoplev.shuvalova_material_design
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.konoplev.shuvalova_material_design.ui.theme.Shuvalova_Material_DesignTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Shuvalova_Material_DesignTheme {
+
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Shuvalova_Material_DesignTheme {
+        Greeting("Android")
+    }
+}
